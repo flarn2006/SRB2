ef6c403cdb2e71519724e731852bc5076510f082
flarn2006 <flarn2006@gmail.com>
2021-06-01 23:59:21 -0400

    add more functionality to repl.lua (interval,spawn)

--- repl.lua
+++ repl.lua
@@ -24,8 +24,109 @@ local env_meta = {
 	end
 }
 
+local interval_meta = {
+	__tostring=function(int)
+		local str
+		if int.enabled then
+			str = '<I='
+		else
+			str = '<i='
+		end
+		return str..tostring(int.time)..': '..tostring(int.str)..'>'
+	end,
+	__call=function(int, ...)
+		local s,r = pcall(int.func, ...)
+		if s then
+			return r
+		else
+			int.enabled = false
+			print_error(r)
+		end
+	end
+}
+
+local list_meta = {
+	__tostring=function(list)
+		if #list == 0 then
+			return '(empty list)'
+		else
+			local str = '\n'
+			for i,v in ipairs(list) do
+				str = $..tostring(i)..'\t'..tostring(v)..'\n'
+			end
+			return str
+		end
+	end
+}
+
+local function process_func_arg(arg, arg_desc)
+	if type(arg) == 'string' then
+		local f = loadstring(arg)
+		if type(f) == 'function' then
+			setfenv(f, env)
+		else
+			error(f)
+		end
+		return f
+	elseif type(arg) == 'function' then
+		return arg
+	else
+		error('Must specify a string or a function as '..arg_desc)
+	end
+end
+
 local function reset_env()
-	env = {out={}}
+	env = {out={}, intervals={}}
+	setmetatable(env.intervals, list_meta)
+
+	env.interval = function(time, func)
+		local str = tostring(func)
+		if type(time) ~= 'number' then
+			error('Must specify a number as the first argument to interval')
+		end
+		func = process_func_arg(func, 'the second argument to interval')
+
+		local int = {time=time, func=func, enabled=true, timeleft=time, str=str}
+
+		int.on = function() int.enabled = true end
+		int.off = function() int.enabled = false end
+		
+		int.append = function(func2)
+			local str2 = tostring(func2)
+			func2 = process_func_arg(func2, 'the argument to append')
+			int.str = $..'; '..str2
+			local oldfunc = int.func
+			int.func = function()
+				oldfunc()
+				func2()
+			end
+		end
+
+		setmetatable(int, interval_meta)
+		table.insert(env.intervals, int)
+		return int
+	end
+
+	env.spawn = function(mt, dist, z)
+		if dist == nil then
+			dist = 128
+		elseif dist >= FRACUNIT then
+			dist = $/FRACUNIT
+		end
+
+		if z == nil then
+			z = 0
+		elseif z < FRACUNIT then
+			z = $*FRACUNIT
+		end
+
+		local x = dist * cos(consoleplayer.mo.angle)
+		local y = dist * sin(consoleplayer.mo.angle)
+		local mo = P_SpawnMobjFromMobj(consoleplayer.mo, x, y, z, mt)
+		mo.angle = ANGLE_180 + consoleplayer.mo.angle
+		return mo
+	end
+
 	setmetatable(env, env_meta)
 end
 reset_env()
@@ -68,3 +169,16 @@ COM_AddCommand('lua', function(player, ...)
 end, COM_LOCAL)
 
 COM_AddCommand('lua_reset', reset_env, COM_LOCAL)
+
+addHook('ThinkFrame', function()
+	for i,v in ipairs(env.intervals) do
+		if v.enabled then
+			if v.timeleft == 1 then
+				v()
+				v.timeleft = v.time
+			else
+				v.timeleft = $-1
+			end
+		end
+	end
+end)
