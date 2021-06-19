13b301fb98aabbbe4266f3c2b2d03c31a5d3108e
flarn2006 <flarn2006@gmail.com>
2021-06-19 00:08:14 -0400

    Remove install.sh from repo
    This is so it won't go away when I change to a branch without it.
    This reverts commit 4e858ebfb3538eae3d538d353e73e5f8d7b42451.

--- install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh -ev
-pushd src
-make $*
-popd
-sudo install -Dm755 bin/Linux64/Release/lsdl2srb2 /usr/local/bin/srb2
-sudo install -Dm644 src/sdl/SDL_icon.xpm /usr/local/share/pixmaps/srb2.xpm
